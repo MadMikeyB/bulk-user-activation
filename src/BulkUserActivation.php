a3c1fe1e9e3994b0d2a726b83dc8a3c2e84876a1
Michael Burton <mikey@burnthebook.co.uk>
2025-01-24 11:22:45 +0000

    EVENT_REGISTER_UTILITY_TYPES -> EVENT_REGISTER_UTILITIES

--- src/BulkUserActivation.php
+++ src/BulkUserActivation.php
@@ -121,7 +121,7 @@ class BulkUserActivation extends Plugin
         // Register our utilities
         Event::on(
             Utilities::class,
-            Utilities::EVENT_REGISTER_UTILITY_TYPES,
+            Utilities::EVENT_REGISTER_UTILITIES,
             function (RegisterComponentTypesEvent $event) {
                 $event->types[] = BulkUserActivationUtilityUtility::class;
             }
